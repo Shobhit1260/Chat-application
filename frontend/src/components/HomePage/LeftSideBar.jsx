eed18bb6f969f441bc83cec274dd9d724a143e20
Shobhit Srivastava <shobhitsri1405@gmail.com>
2025-07-27 22:30:13 +0530

    enhance group creation UI with form and user selection functionality

--- frontend/src/components/HomePage/LeftSideBar.jsx
+++ frontend/src/components/HomePage/LeftSideBar.jsx
@@ -30,27 +30,40 @@ function LeftSideBar({users,socket,onlineUserIds}) {
             <img src={Menu} alt="menu" onClick={()=>setMenu(!menu)}/>
             {menu ? 
              <div className='z-40 absolute top-0 right-5 p-4 rounded-lg backdrop-blur bg-white flex flex-col gap-1 w-max h-min'>
+              
               <h1 className='font-serif text-sm text-gray-600 cursor-default' onClick={()=>logout()}>logout</h1> 
               <h2 className='font-serif text-sm text-gray-600 cursor-default' onClick={()=>setShowUsers(!showUsers)}>Create group</h2>
              </div>:null
             }
             {showUsers?
-            <div className='fixed inset-0 p-4 bg-white/20 bg-opacity-10 flex flex-col justify-center items-center overflow-y-scroll'>
-           <div className="z-40 w-1/2 h-min p-4 bg-white flex flex-col gap-8 justify-center items-center relative rounded-xl">
+            <div className='z-40 fixed inset-0 p-4 bg-white/20 bg-opacity-10 flex flex-col justify-center items-center overflow-y-scroll'>
+           <div className=" w-1/2 h-min py-4 px-8  bg-white flex flex-col gap-8 justify-center items-start relative rounded-xl">
+           <h1 className='text-2xl font-bold text-black mx-auto'>Create a new group</h1>
+           <form action="">
+            <label htmlFor="groupName">Group Name:</label>
+            <input id="groupName" type="text" />
            {users.map((user)=>{
             return (
-               console.log("user:",users.length),
-                    <div key={user._id} className={`flex justify-start items-center 
+                    <div key={user._id} className={`w-full flex justify-center items-between gap-4
                        `}>
+                      {/* <label htmlFor="userName">  */}
+                      <div className='w-3/4 flex justify-start items-center gap-4'>
                       <div className="w-8 aspect-[1/1] rounded-full overflow-hidden">
                         <img src={user.picture} alt="profile" className="w-full h-full object-cover" />
                       </div>
                       <div className="font-sm text-black">{user.nickname}</div>
-                        
-                    </div>   
+                      </div> 
+                      {/* </label>  */}
+                      <input  id="userName"  className='w-1/4 text-xl' type="checkbox" />
+                       
+                    </div>  
+
             )
           })
-          }</div> 
+          }
+          <input type="submit" value="Create Group" className='mx-auto p-4 bg-green-600 hover:bg-green-700' />
+          </form>
+          </div> 
        </div>:null     
           }
          </div>
